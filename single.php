c85d78d89a73a87397e377bf7b244210e4bfaf47
tamura satoshi <stamura0926@gmail.com>
2025-06-15 13:00:28 +0900

    記事データの有無の確認追加

--- single.php
+++ single.php
@@ -4,18 +4,21 @@ get_header();
 <main>
     <section id="main-content">
     <?php
-    if ( have_posts() ) :
-        while ( have_posts() ) : the_post();
-            // タイトルの出力
-            $contents_name = SCF::get('content_name');
-            if ( $contents_name ) {
-                echo '<h2>'.esc_html($contents_name).'</h2>';
-            }
+    if ( have_posts() ) {
+        // 投稿データをセットアップ
+        the_post();
 
-            // 本文の出力
-            the_content();
-        endwhile;
-    endif;
+        // タイトルの出力
+        $contents_name = SCF::get('content_name');
+        if ( $contents_name ) {
+            echo '<h2>'.esc_html($contents_name).'</h2>';
+        }
+
+        // 本文の出力
+        the_content();
+    } else {
+        echo '<p>記事が見つかりませんでした。</p>';
+    }
     ?>
     </section>
 </main>
