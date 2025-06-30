a8203a49b02f1f85887f2f5ec1ffb444d37c98d4
tamura satoshi <stamura0926@gmail.com>
2025-07-01 00:20:56 +0900

    ページ切り替え時の不具合対応

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * The base (and only) template
+ *
+ * @package WordPress
+ * @subpackage intentionally-blank
+ */
+get_header();
+require_once('main-content.php');
+get_footer(); 
+?>
+
